379191303efb3721d98bd30a97edc47466417910
Rafayel Azizyan <rafayel.azizyan@gmail.com>
2020-09-23 01:02:34 +0200

    Collegues #2 -Version-1 corr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { C2Component } from './c2/c2.component';
 import { CollegueComponent } from './collegue/collegue.component';
 
 @NgModule({
   // la liste des composants graphiques du module
   declarations: [
     AppComponent,
-    C2Component,
     CollegueComponent
   ],
   // dépendance vers d'autres modules
